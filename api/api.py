a0c6026c64a819d6264dcc70ef6cb8e26d65ae40
SorinCR <xxsoringt@gmail.com>
2022-12-20 12:06:19 +0200

    pymongo + requirements

--- api/api.py
+++ api/api.py
@@ -1,7 +1,10 @@
 import time
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
 
 @app.route('/time')
 def get_current_time():
