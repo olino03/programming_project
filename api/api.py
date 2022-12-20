e56abd59c0264a11d2f2d371dfa3cddf913169d3
SorinCR <xxsoringt@gmail.com>
2022-12-20 12:27:16 +0200

    MongoEngine

--- api/api.py
+++ api/api.py
@@ -1,11 +1,30 @@
 import time
 from flask import Flask
-from pymongo import MongoClient
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'programare',
+    'host': 'localhost',
+    'port': 27017
+}
+
+db = MongoEngine()
+db.init_app(app)
+
+class User(db.Document):
+    name = db.StringField()
+    email = db.StringField()
+
+class Restante(db.Document):
+    materii = db.ListField()
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/test')
+def writeDB():
+    Restante(materii=["mate", "logica"]).save()
+    return {'success': True}
