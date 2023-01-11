e8ac398ca71a309633d2f099e1454557da919d4b
SorinCR <xxsoringt@gmail.com>
2023-01-11 02:18:09 +0200

    register ca atat s-a putut azi

--- api/api.py
+++ api/api.py
@@ -1,6 +1,10 @@
 import time
-from flask import Flask
+import bcrypt
+from flask import Flask, request
 from flask_mongoengine import MongoEngine
+# from os import environ
+
+# secret = environ.get('SECRET')
 
 app = Flask(__name__)
 
@@ -13,18 +17,34 @@ app.config['MONGODB_SETTINGS'] = {
 db = MongoEngine()
 db.init_app(app)
 
-class User(db.Document):
-    name = db.StringField()
-    email = db.StringField()
-
-class Restante(db.Document):
-    materii = db.ListField()
+# class Restante(db.Document):
+#     materii = db.ListField()
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
 
-@app.route('/test')
-def writeDB():
-    Restante(materii=["mate", "logica"]).save()
-    return {'success': True}
+# @app.route('/test')
+# def writeDB():
+#     Restante(materii=["mate", "logica"]).save()
+#     return {'success': True}
+
+class User(db.Document):
+    fname = db.StringField()
+    lname = db.StringField()
+    email = db.StringField()
+    phone = db.StringField()
+    password = db.StringField()
+
+@app.route('/register', methods=["POST"])
+def register():
+    data = request.json
+    print(data)
+    passw = bytes(data['password'], 'UTF-8')
+
+    salt = bcrypt.gensalt()
+    hashedPass = bcrypt.hashpw(passw, salt)
+    print(hashedPass)
+
+    User(fname=data['fname'], lname=data['lname'], email=data['email'], phone=data["phone"], password=hashedPass).save()
+    return {"success": True}
